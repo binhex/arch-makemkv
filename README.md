3b83cca069dc63f1c078e8f2f2b765e4b03b0034
binhex <megalith01@gmail.com>
2019-06-20 22:01:30 +0100

    initial push

---
+++ README.md
@@ -0,0 +1,79 @@
+**Application**
+
+[makemkv](https://makemkv.com/)
+
+**Description**
+
+MakeMKV is your one-click solution to convert video that you own into free and patents-unencumbered format that can be played everywhere. MakeMKV is a format converter, otherwise called "transcoder". It converts the video clips from proprietary (and usually encrypted) disc into a set of MKV files, preserving most information but not changing it in any way. The MKV format can store multiple video/audio tracks with all meta-information and preserve chapters. There are many players that can play MKV files nearly on all platforms, and there are tools to convert MKV files to many formats, including DVD and Blu-ray discs.
+
+**Build notes**
+
+Latest stable makemkv release from Arch Linux.
+
+**Usage**
+```
+docker run -d \
+    -p 5900:5900 \
+    -p 6080:6080 \
+    --name=<container name> \
+    --privileged=true \
+    -v <path for config files>:/config \
+    -v /etc/localtime:/etc/localtime:ro \
+    -e WEBPAGE_TITLE=<name shown in browser tab> \
+    -e VNC_PASSWORD=<password for web ui> \
+    -e UMASK=<umask for created files> \
+    -e PUID=<uid for user> \
+    -e PGID=<gid for user> \
+    binhex/arch-makemkv
+```
+
+Please replace all user variables in the above command defined by <> with the correct values.
+
+**Example**
+```
+docker run -d \
+    -p 5900:5900 \
+    -p 6080:6080 \
+    --name=makemkv \
+    --privileged=true \
+    -v /apps/docker/makemkv:/config \
+    -v /etc/localtime:/etc/localtime:ro \
+    -e WEBPAGE_TITLE=Tower \
+    -e VNC_PASSWORD=mypassword \
+    -e UMASK=000 \
+    -e PUID=0 \
+    -e PGID=0 \
+    binhex/arch-makemkv
+```
+
+If you do specify a password for the web ui via the env var 'VNC_PASSWORD' then it MUST be 6 characters or longer, otherwise it will be ignored.
+
+**Access via web interface (noVNC)**
+
+`http://<host ip>:<host port>/vnc.html?resize=remote&host=<host ip>&port=<host port>&&autoconnect=1`
+
+e.g.:-
+
+`http://192.168.1.10:6080/vnc.html?resize=remote&host=192.168.1.10&port=6080&&autoconnect=1`
+
+**Access via VNC client**
+
+`<host ip>::<host port>`
+
+e.g.:-
+
+`192.168.1.10::5900`
+
+**Notes**
+
+User ID (PUID) and Group ID (PGID) can be found by issuing the following command for the user you want to run the container as:-
+
+```
+id <username>
+```
+___
+If you appreciate my work, then please consider buying me a beer  :D
+
+[![PayPal donation](https://www.paypal.com/en_US/i/btn/btn_donate_SM.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=MM5E27UX6AUU4)
+
+[Support forum](https://lime-technology.com/forums/topic/71764-support-binhex-makemkv/)
---
+++ build/root/install.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+
+# exit script if return code != 0
+set -e
+
+# build scripts
+####
+
+# download build scripts from github
+curl --connect-timeout 5 --max-time 600 --retry 5 --retry-delay 0 --retry-max-time 60 -o /tmp/scripts-master.zip -L https://github.com/binhex/scripts/archive/master.zip
+
+# unzip build scripts
+unzip /tmp/scripts-master.zip -d /tmp
+
+# move shell scripts to /root
+mv /tmp/scripts-master/shell/arch/docker/*.sh /root/
+
+# pacman packages
+####
+
+# call pacman db and package updater script
+source /root/upd.sh
+
+# define pacman packages
+pacman_packages=""
+
+# install compiled packages using pacman
+if [[ ! -z "${pacman_packages}" ]]; then
+	pacman -S --needed $pacman_packages --noconfirm
+fi
+
+# aur packages
+####
+
+# define aur packages
+aur_packages="makemkv"
+
+# call aur install script (arch user repo)
+source /root/aur.sh
+
+# config novnc
+###
+
+# overwrite novnc 16x16 icon with application specific 16x16 icon (used by bookmarks and favorites)
+cp /home/nobody/novnc-16x16.png /usr/share/novnc/app/images/icons/
+
+cat <<'EOF' > /tmp/startcmd_heredoc
+# launch makemkv (we cannot simply call /usr/bin/makemkv otherwise it wont run on startup)
+# note failure to launch makemkv in the below manner will result in the classic xcb missing error
+dbus-run-session -- makemkv
+EOF
+
+# replace startcmd placeholder string with contents of file (here doc)
+sed -i '/# STARTCMD_PLACEHOLDER/{
+	s/# STARTCMD_PLACEHOLDER//g
+	r /tmp/startcmd_heredoc
+}' /home/nobody/start.sh
+rm /tmp/startcmd_heredoc
+
+# config openbox
+####
+
+cat <<'EOF' > /tmp/menu_heredoc
+	<item label="makemkv">
+	<action name="Execute">
+	  <command>dbus-launch makemkv</command>
+	  <startupnotify>
+		<enabled>yes</enabled>
+	  </startupnotify>
+	</action>
+	</item>
+EOF
+
+# replace menu placeholder string with contents of file (here doc)
+sed -i '/<!-- APPLICATIONS_PLACEHOLDER -->/{
+	s/<!-- APPLICATIONS_PLACEHOLDER -->//g
+	r /tmp/menu_heredoc
+}' /home/nobody/.config/openbox/menu.xml
+rm /tmp/menu_heredoc
+
+# env vars
+####
+
+
+# container perms
+####
+
+# define comma separated list of paths
+install_paths="/tmp,/usr/share/themes,/home/nobody,/usr/share/novnc,/usr/share/applications,/etc/xdg"
+
+# split comma separated string into list for install paths
+IFS=',' read -ra install_paths_list <<< "${install_paths}"
+
+# process install paths in the list
+for i in "${install_paths_list[@]}"; do
+
+	# confirm path(s) exist, if not then exit
+	if [[ ! -d "${i}" ]]; then
+		echo "[crit] Path '${i}' does not exist, exiting build process..." ; exit 1
+	fi
+
+done
+
+# convert comma separated string of install paths to space separated, required for chmod/chown processing
+install_paths=$(echo "${install_paths}" | tr ',' ' ')
+
+# set permissions for container during build - Do NOT double quote variable for install_paths otherwise this will wrap space separated paths as a single string
+chmod -R 775 ${install_paths}
+
+# create file with contents of here doc, note EOF is NOT quoted to allow us to expand current variable 'install_paths'
+# we use escaping to prevent variable expansion for PUID and PGID, as we want these expanded at runtime of init.sh
+cat <<EOF > /tmp/permissions_heredoc
+
+# get previous puid/pgid (if first run then will be empty string)
+previous_puid=\$(cat "/tmp/puid" 2>/dev/null || true)
+previous_pgid=\$(cat "/tmp/pgid" 2>/dev/null || true)
+
+# if first run (no puid or pgid files in /tmp) or the PUID or PGID env vars are different
+# from the previous run then re-apply chown with current PUID and PGID values.
+if [[ ! -f "/tmp/puid" || ! -f "/tmp/pgid" || "\${previous_puid}" != "\${PUID}" || "\${previous_pgid}" != "\${PGID}" ]]; then
+
+	# set permissions inside container - Do NOT double quote variable for install_paths otherwise this will wrap space separated paths as a single string
+	chown -R "\${PUID}":"\${PGID}" ${install_paths}
+
+fi
+
+# write out current PUID and PGID to files in /tmp (used to compare on next run)
+echo "\${PUID}" > /tmp/puid
+echo "\${PGID}" > /tmp/pgid
+
+# env var required to find qt plugins when starting makemkv
+export QT_QPA_PLATFORM_PLUGIN_PATH=/usr/lib/qt/plugins/platforms
+
+# env vars required to enable menu icons for makemkv (also requires breeze-icons package)
+export KDE_SESSION_VERSION=5 KDE_FULL_SESSION=true
+EOF
+
+# replace permissions placeholder string with contents of file (here doc)
+sed -i '/# PERMISSIONS_PLACEHOLDER/{
+	s/# PERMISSIONS_PLACEHOLDER//g
+	r /tmp/permissions_heredoc
+}' /root/init.sh
+rm /tmp/permissions_heredoc
+
+# env vars
+####
+
+# cleanup
+yes|pacman -Scc
+pacman --noconfirm -Rns $(pacman -Qtdq) 2> /dev/null || true
+rm -rf /usr/share/locale/*
+rm -rf /usr/share/man/*
+rm -rf /usr/share/gtk-doc/*
+rm -rf /tmp/*
